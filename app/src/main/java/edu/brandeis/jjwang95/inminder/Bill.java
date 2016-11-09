78289c43c4183a11b58f4eed9aeed84478eca967
Jingjing Wang <jingwin@outlook.com>
2016-11-08 22:09:03 -0500

    main activity

---
+++ app/src/main/java/edu/brandeis/jjwang95/inminder/Bill.java
@@ -0,0 +1,13 @@
+package edu.brandeis.jjwang95.inminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bill extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill);
+    }
+}
--- app/src/main/java/edu/brandeis/jjwang95/inminder/MainActivity.java
+++ app/src/main/java/edu/brandeis/jjwang95/inminder/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.brandeis.jjwang95.inminder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button rem_button = (Button) findViewById(R.id.reminder_button);
+        Button bill_button = (Button) findViewById(R.id.bill_button);
+        Button pass_button = (Button) findViewById(R.id.password_button);
+
+        rem_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent("edu.brandeis.jjwang95.inminder.Reminder"));
+            }
+        });
+
+        bill_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent("edu.brandeis.jjwang95.inminder.Bill"));
+            }
+        });
+
+        pass_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent("edu.brandeis.jjwang95.inminder.Password"));
+            }
+        });
+
+
+
     }
 }
