dae4e017a173958efdef8df81b430a5ddf7654e8
zhengyr <stella.zhengyr@gmail.com>
2016-12-04 02:03:53 -0500

    actionBar and main theme

--- app/src/main/java/edu/brandeis/jjwang95/inminder/MainActivity.java
+++ app/src/main/java/edu/brandeis/jjwang95/inminder/MainActivity.java
@@ -8,22 +8,45 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SimpleCursorAdapter;
-import com.beardedhen.androidbootstrap.TypefaceProvider;
-import com.beardedhen.androidbootstrap.BootstrapButton;
+//import com.beardedhen.androidbootstrap.TypefaceProvider;
+//import com.beardedhen.androidbootstrap.BootstrapButton;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private DBHelper dbHelper;
     private SimpleCursorAdapter dataAdapter;
     private SQLiteDatabase db;
-
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TypefaceProvider.registerDefaultIconSets();
+//        TypefaceProvider.registerDefaultIconSets();
         setContentView(R.layout.activity_main);
-        BootstrapButton rem_button = (BootstrapButton) findViewById(R.id.reminder_button);
-        BootstrapButton bill_button = (BootstrapButton) findViewById(R.id.bill_button);
-        BootstrapButton pass_button = (BootstrapButton) findViewById(R.id.password_button);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+//        BootstrapButton rem_button = (BootstrapButton) findViewById(R.id.reminder_button);
+//        BootstrapButton bill_button = (BootstrapButton) findViewById(R.id.bill_button);
+//        BootstrapButton pass_button = (BootstrapButton) findViewById(R.id.password_button);
 
         /*********************Test*******************************************************/
 //        Log.d("Start", "SHOW");
@@ -44,26 +67,61 @@ public class MainActivity extends AppCompatActivity {
 //        dbHelper.closeDB();
         /*********************Test*******************************************************/
 
-        rem_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent("edu.brandeis.jjwang95.inminder.Reminder"));
-            }
-        });
+//        rem_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent("edu.brandeis.jjwang95.inminder.Reminder"));
+//            }
+//        });
+//
+//        bill_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent("edu.brandeis.jjwang95.inminder.Bill"));
+//            }
+//        });
+//
+//        pass_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent("edu.brandeis.jjwang95.inminder.Password"));
+//            }
+//        });
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Reminder(), "Reminder");
+//        adapter.addFragment(new TwoFragment(), "TWO");
+//        adapter.addFragment(new ThreeFragment(), "THREE");
+        viewPager.setAdapter(adapter);
+    }
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
 
-        bill_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent("edu.brandeis.jjwang95.inminder.Bill"));
-            }
-        });
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
 
-        pass_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent("edu.brandeis.jjwang95.inminder.Password"));
-            }
-        });
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 
 }
--- app/src/main/java/edu/brandeis/jjwang95/inminder/Reminder.java
+++ app/src/main/java/edu/brandeis/jjwang95/inminder/Reminder.java
@@ -5,41 +5,52 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 
-public class Reminder extends AppCompatActivity {
+public class Reminder extends Fragment {
     private DBHelper dbHelper;
     private SimpleCursorAdapter reAdapter;
     private SQLiteDatabase db;
     ListView listview;
     Button detailBtn, addBtn;
     int request_Code;
+    View rootView;
+    public Reminder() {
+        // Required empty public constructor
+    }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reminder);
-        dbHelper = DBHelper.getInstance(getApplicationContext());
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        rootView = inflater.inflate(R.layout.activity_reminder, container, false);
+        listview = (ListView) rootView.findViewById(R.id.reminder_list);
+        addBtn = (Button) rootView.findViewById(R.id.addBtn);
+
+
+
+        dbHelper = DBHelper.getInstance(getActivity());
         dbHelper.onOpen(db);
         Cursor cursor = dbHelper.getAllReminders();
 
         String[] keys = new String[]{"name","date"};
         int[] boundTo = new int[]{R.id.nameShow, R.id.timeShow};
-        listview = (ListView) findViewById(R.id.reminder_list);
 
-        addBtn = (Button) findViewById(R.id.addBtn);
         addBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent intent = new Intent("edu.brandeis.jjwang95.inminder.AddReminder");
                 startActivityForResult(intent,request_Code);
-                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                getActivity().overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
         });
 
-        reAdapter = new SimpleCursorAdapter(this, R.layout.reminder_entry, cursor, keys, boundTo, 0) {
+        reAdapter = new SimpleCursorAdapter(getActivity(), R.layout.reminder_entry, cursor, keys, boundTo, 0) {
             public View getView(final int position, View view, ViewGroup parent) {
                 View myView = super.getView(position, view, parent);
                 detailBtn = (Button) myView.findViewById(R.id.detailBtn);
@@ -47,22 +58,32 @@ public class Reminder extends AppCompatActivity {
                     public void onClick(View v){
                         Cursor curr = (Cursor)getItem(position);
                         int id = curr.getInt(curr.getColumnIndex("_id"));
-                        Intent intent = new Intent(Reminder.this, ReminderDetail.class);
+                        Intent intent = new Intent(getActivity(), ReminderDetail.class);
                         intent.putExtra("id",id);
                         startActivityForResult(intent,request_Code);
-                        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                        getActivity().overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
                     }
                 });
                 return myView;
             }
         };
         listview.setAdapter(reAdapter);
+
+
+
+
+        return rootView;
+    }
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data){
         if (requestCode == request_Code){
-            if (resultCode == RESULT_OK) {
-                dbHelper = DBHelper.getInstance(getApplicationContext());
+            if (resultCode == getActivity().RESULT_OK) {
+                dbHelper = DBHelper.getInstance(getActivity());
                 dbHelper.onOpen(db);
                 reAdapter.changeCursor(dbHelper.getAllReminders());
             }
